493a68eeea03195441aa2b42897c8db83a6c9eef
Ubuntu <vmadmin@ubuntu-gitlabstack01.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-09-28 19:35:00 +0000

    >>

--- cronjobs.sh
+++ cronjobs.sh
@@ -1,3 +1,13 @@
-echo "hello from cronjobs.sh" >> /var/log/cron.log
-docker ps >> /var/log/cron.log
+
+echo "Started at $(date)" >> /var/log/cron.log
+echo "running containers are $(docker ps -q)" >> /var/log/cron.log
+echo "running volumes are" >> /var/log/cron.log
+for volume in $(docker volume ls -q); do
+  c=$(docker ps --filter volume=$volume -q|wc -l)
+  if [ $c -eq  1 ]; then
+    echo $volume >> /var/log/cron.log
+  fi
+done
+
+cat /var/log/cron.log
 
