4fc1654b0976b285405cbaa011d5d04795ad2507
Ubuntu <vmadmin@ubuntu-gitlabstack01.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-09-28 20:17:46 +0000

    update cronjobs.sh

--- cronjobs.sh
+++ cronjobs.sh
@@ -1,7 +1,6 @@
-
+echo " " >> /var/log/cron.log
 echo "Started at $(date)" >> /var/log/cron.log
-echo "running containers are $(docker ps -q)" >> /var/log/cron.log
-echo "running volumes are" >> /var/log/cron.log
+echo "backups for volumes for running containers on $(hostname)" >> /var/log/cron.log
 for volume in $(docker volume ls -q); do
   c=$(docker ps --filter volume=$volume -q|wc -l)
   if [ $c -eq  1 ]; then
