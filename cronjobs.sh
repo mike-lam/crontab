74c76a6ef100b228029be212d54fdd39b9e526aa
Ubuntu <vmadmin@ubuntu-gitlabstack01.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-10-01 13:46:49 +0000

    use sleep instead

--- cronjobs.sh
+++ cronjobs.sh
@@ -1,12 +1,21 @@
-echo " " >> /var/log/cron.log
-echo "Started at $(date)" >> /var/log/cron.log
-echo "backups for volumes for running containers on $(hostname)" >> /var/log/cron.log
-for volume in $(docker volume ls -q); do
-  c=$(docker ps --filter volume=$volume -q|wc -l)
-  if [ $c -eq  1 ]; then
-    echo $volume >> /var/log/cron.log
-  fi
-done
 
-cat /var/log/cron.log
+# set in docker file insetad as an ENV var SLEEP=15s  #can be in s, m, h, d
+
+create_backups() {
+  echo "Started at $(date)" >> /var/log/cron.log
+  echo "running containers are $(docker ps -q)" >> /var/log/cron.log
+  echo "running volumes are" >> /var/log/cron.log
+  for volume in $(docker volume ls -q); do
+    c=$(docker ps --filter volume=$volume -q|wc -l)
+    if [ $c -eq  1 ]; then
+      echo $volume >> /var/log/cron.log
+    fi
+  done
+  }
+
+for (( ; ; )); do
+  create_backups
+  cat /var/log/cron.log
+  sleep $SLEEP
+done
 
