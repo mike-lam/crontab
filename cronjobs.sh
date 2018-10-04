9c04ca5e6a621c18997e6513ed03765f4507e06c
Ubuntu <vmadmin@ubuntu-gitlabstack01.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-10-04 15:12:11 +0000

    adding deleting old backups

--- cronjobs.sh
+++ cronjobs.sh
@@ -78,12 +78,12 @@ create_backups() {
 
 delete_old_backups() {
   #delete backup dirs older then $DELETE_MTIME, also keep only last $DELETE_LOG_SIZE lines of delete logs
-  cp $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/cron.log $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/crontmp.log
-  echo "Started delete_old_backups on $(hostname) at $(date)"  2>&1 | tee  $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/crontmp.log
-  find $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/ -mtime +$DELETE_MTIME 2>&1 | tee -a $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/crontmp.log
-  find $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/ -mtime +$DELETE_MTIME -exec rm -r {} \;
-  echo "DONE with delete_old_backups at $(date)!"  2>&1 | tee -a $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/crontmp.log
-  tail -n $DELETE_LOG_SIZE  $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/crontmp.log >  $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/cron.log
+  cp $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/cron.log $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
+  echo "Started delete_old_backups on $(hostname) at $(date)"  2>&1 | tee  $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
+  find $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER -mtime +$DELETE_MTIME 2>&1 | tee -a $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
+  find $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER -mtime +$DELETE_MTIME -exec rm -r {} \;
+  echo "DONE with delete_old_backups at $(date)!"  2>&1 | tee -a $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log
+  tail -n $DELETE_LOG_SIZE  $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER/crontmp.log >  $DOCKER_ROOT_DIR/volumes/"$NAMESPACE"_ftp/_data/$FTP_USER/cron.log
 }
  
 sleep $SLEEP_INIT  #give other container some lead time to start running
